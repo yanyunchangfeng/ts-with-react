a0e414412e0bf2c5e6e286b55fab7071e720d37f
System Administrator <yanyunchangfeng@gmail.com>
2020-06-07 11:32:55 +0800

    add useRef

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,34 @@ import LikeButton from './components/LikeButton';
 import MouseTracker from './components/MouseTracker';
 import ClassLike from './components/classLike';
 import useMousePosition from './hooks/useMousePosition';
+import useURLLoader from './hooks/useURLLoader';
 import './App.css';
 interface IShowResult {
    message:string;
    status:string;
 }
-const DogShow:React.FC<{data:IShowResult}> = ({data})=>{
-   return(
-     <>
-       <h2>Dog show:{data.status}</h2>
-       <img src={data.message}/>
-     </>
-   )
-}
+// const DogShow:React.FC<{data:IShowResult}> = ({data})=>{
+//    return(
+//      <>
+//        <h2>Dog show:{data.status}</h2>
+//        <img src={data.message}/>
+//      </>
+//    )
+// }
 const  App:React.FC = () => {
   const [show,setShow] = useState(true)
   const positions = useMousePosition()
+  const [data,loading]  = useURLLoader('https://dog.ceo/api/breeds/image/random',[show])
+  const DogResult = data as IShowResult;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <button onClick={()=>{setShow(!show)}}>Toggle Tracker</button>
+          <button onClick={()=>{setShow(!show)}}>Refresh Dog Photo</button>
         </p>
         <p>X:{positions.x} Y:{positions.y}</p>
+        {loading?<p>读取中</p>:<img src={DogResult&&DogResult.message}/>}
         <Hello message="Hello World"/>
         <ClassLike/>
         <LikeButton/>
--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from 'react'
+import React, {useState,useEffect, useRef} from 'react'
 import useMousePosition from '../hooks/useMousePosition'
 const LikeButton:React.FC =()=>{
     // const [like,setLike] = useState(0)
@@ -7,6 +7,10 @@ const LikeButton:React.FC =()=>{
     const [like,setLike] = useState(0)
     const [on,setOn] = useState(true)
     const positions = useMousePosition()
+    const likeRef = useRef(0)
+    const didUpdateRef = useRef(false);// 模拟生命周期componentDidUpdate
+    const domRef = useRef<HTMLInputElement>(null)
+    // {current:12} ref 和直接一个对象有什么区别 ref在所有的render当中都保持着唯一的引用 因此对ref的取值赋值 拿到的都是最终的状态
     //空节点包含两个button
     // return (
     //     <>
@@ -22,15 +26,31 @@ const LikeButton:React.FC =()=>{
     //     </button>
     //     </>
     // )
+    function handleClick(){
+        setTimeout(()=>{
+          alert(`you clicked on ${likeRef.current}`)
+        },3000)
+    }
     useEffect(()=>{
        console.log('document title effect is running')
+       if(didUpdateRef.current){
+         console.log('componentDidUpdate')
+       }else{
+           didUpdateRef.current = true
+       }
        document.title = `点击了${like}次`
     },[like,on])
+    useEffect(()=>{
+        // console.log('rerender')
+       domRef.current?.focus() 
+    })
         return (
         <>
+        <input type="text" ref={domRef}/>
         <h2>X:{positions.x} Y:{positions.y}</h2>
             <button onClick ={()=>{ 
-                setLike(like+1)
+                setLike(like+1);
+                likeRef.current ++
             }}>
                 {like}
             </button>
@@ -38,6 +58,10 @@ const LikeButton:React.FC =()=>{
                 setOn(!on)
             }}>
                 {on?'ON':'OFF'}
+            </button>
+            <button onClick ={ handleClick
+            }>
+              Alert！
             </button>
         </>
     )
